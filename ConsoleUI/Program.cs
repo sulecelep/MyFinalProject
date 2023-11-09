ea63514f51bf72c39368f08afe334a25edf18a99
ssule <ssule@CELEP>
2023-11-09 13:48:17 +0300

    10. gün sonu

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Business.Concrete;
 using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace ConsoleUI
 {
@@ -26,10 +27,19 @@ namespace ConsoleUI
         private static void ProductTest()
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
-            foreach (var product in productManager.GetProductDetails())
+            var result = productManager.GetProductDetails();
+            if(result.Success==true)
             {
-                Console.WriteLine(product.ProductName+ " / "+ product.CategoryName);
+                foreach (var product in result.Data)
+                {
+                    Console.WriteLine(product.ProductName + " / " + product.CategoryName);
+                }
             }
+            else
+            {
+                Console.WriteLine(result.Message);
+            }
+            
         }
     }
 }
