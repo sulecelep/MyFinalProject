dd507354e618dd9ee57b2dec1b417c38c2b54011
ssule <ssulecelepp@gmail.com>
2023-12-01 20:00:23 +0300

    11. gün yarısı tamamlandı.

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        //naming convention
+        //IoC Container -- Inversion of Control
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public List<Product> Get()
+        {
+            var result = _productService.GetAll();
+            return result.Data;
+        }
+    }
+}
